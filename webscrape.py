68bea63037a4a1e73ccdb529d5063026e3bae608
nikvijay07 <nikvijay07@gmail.com>
2024-02-10 16:47:41 -0500

    setting up flask server

---
+++ webscrape.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def scrape():
+    # Receive data from Selenium script
+    # data = request.json
+    
+    # Process data (e.g., scrape website)
+    # ...
+
+    # Return scraped data as response
+    print("hello")
+    return {'result': 'Scraped data'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
